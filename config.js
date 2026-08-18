edfdba44c1ba09e9ed88215d7149877b22718500
849276022 <59733146+849276022@users.noreply.github.com>
2026-08-18 12:21:45 +0800

    fix: 更新正确的后端地址

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   // 后端公网地址
-  API_BASE_URL: 'https://express-zl1o-002.sh.run.tcloudbase.com',
+  API_BASE_URL: 'https://express-zl1o-257761-8-1433125757.sh.run.tcloudbase.com',
   USE_DEMO_DATA: false,
   POLL_INTERVAL_MS: 5000
 }
